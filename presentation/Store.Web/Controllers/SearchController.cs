0a14ff803011fd941982ff689e972dd94e2a3c05
EvgeniyJarsky <you@example.com>
2024-02-10 19:54:03 +0300

    Добавить станицу поиска

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepository bookRepositiry;
+
+        public SearchController(IBookRepository bookRepositiry)
+        {
+            this.bookRepositiry = bookRepositiry;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var books  = bookRepositiry.GetAllByTitle(query);
+
+            return View(books);
+        }
+    }
+}
